1c40c99a7edc474a9c2cea466068eed37d744442
DNN13 <dtn1327@gmail.com>
2024-08-07 22:44:30 +0700

    Update node_send.ino

--- node_send/node_send.ino
+++ node_send/node_send.ino
@@ -2,7 +2,10 @@
 #include <Wire.h>
 #include <esp_now.h>
 #include <WiFi.h>
+#include <DHT.h>
+#define DHT11_PIN 14
 
+DHT dht(DHT11_PIN, DHT11);
 // uint8_t broadcastAddress[] = {0x30, 0xC9, 0x22, 0x32, 0x5D, 0x30};
 // uint8_t broadcastAddress[] = {0x24, 0x0A, 0xC4, 0x00, 0x01, 0x00};
 uint8_t broadcastAddress[] = {0xD0, 0xEF, 0x76, 0x57, 0xF2, 0x50};
@@ -13,6 +16,8 @@ typedef struct struct_message {
   int seq;
   int mq_data;
   bool fl_data;
+  float temp_data;
+  float humi_data;
 } struct_message;
 
 struct_message myData;
@@ -27,11 +32,13 @@ bool buz_value = false;
 bool dcm_value = false;
 bool fl_value;
 int mq_value;
+float temp_value;
+float humi_value;
 
 void setup() {
   // lcd.init();
   // lcd.backlight();
-
+  dht.begin();
   pinMode(MQ, INPUT);
   pinMode(FL, INPUT);
   pinMode(dcm, OUTPUT);
@@ -61,7 +68,8 @@ void loop() {
   myData.seq = 101;
   myData.mq_data = mq_value;
   myData.fl_data = fl_value;
-  
+  myData.temp_data = temp_value;
+  myData.humi_data = humi_value;
   // Send message via ESP-NOW
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
   if (result == ESP_OK) {
@@ -77,16 +85,19 @@ void readSensorShow()
   fl_value = digitalRead(FL);
   mq_value = analogRead(MQ);
 
+  humi_value = dht.readHumidity();
+  temp_value = dht.readTemperature();
+
   Serial.print("FL: ");
   Serial.println(fl_value);
   Serial.print("MQ: " );
   Serial.println(mq_value);
-  Serial.print("BUZ: ");
-  Serial.println(buz_value);
-  Serial.print("DCM: " );
-  Serial.println(dcm_value);
+  Serial.print("Temp: ");
+  Serial.println(temp_value);
+  Serial.print("Humi: " );
+  Serial.println(humi_value);
 
-  if (fl_value == 0 || mq_value >= 1000)
+  if (fl_value == 0 || mq_value >= 500)
   {
     dcm_value = true;
     buz_value = true;
